7ee48d94c9a2e9643c49b344a5e60022ef512f78
Bubonqulas <hassandarky@gmail.com>
2024-04-09 13:05:06 -0600

    Made test gui

---
+++ Gui.java
@@ -0,0 +1,49 @@
+import java.awt.BorderLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class gui implements ActionListener {
+    int count = 0;
+    JLabel label;
+
+    public gui() {
+
+        JFrame frame = new JFrame(); // Creates Jframe object
+
+        JPanel panel = new JPanel(); // Creates Jpanel object
+        JButton button = new JButton("Click me"); // Creates Jbutton object
+        button.addActionListener(this);
+        label = new JLabel("Number of clicks: " + count);
+        panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
+        panel.setLayout(new GridLayout(0, 1));
+        panel.add(button);
+        panel.add(label);
+
+        frame.add(panel, BorderLayout.CENTER); // Add panel to frame
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); // Add close operation
+        frame.setTitle("Test Gui"); // Set titel
+        frame.pack();
+        frame.setVisible(true);
+
+        // frame.add(button);
+    }
+
+    public static void main(String[] args) {
+        new gui();
+
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent arg0) {
+        count++;
+        label.setText("Number of clicks: " + count);
+    }
+
+}
